755ebfc8c0040ccb0535de40d8f2635a1d131a9b
SangHyeok0908 <tkdgur9799@gmail.com>
2024-07-20 16:11:42 +0900

    Refactor : before = 처음 카메라 타켓을 서울시청으로 잡고 카메라 업데이트로 현위치로 타켓을 바꿈, after = 처음 로딩화면을 띄운 후 future method가 값을 반환하게 되면 현 위치로 타켓 설정

--- puppy_alert/lib/widgets/child_widgets/food_map_child_widget.dart
+++ puppy_alert/lib/widgets/child_widgets/food_map_child_widget.dart
@@ -3,74 +3,70 @@ import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:geolocator/geolocator.dart';
 
 class FoodMapChildWidget extends StatefulWidget {
-  NMarker marker;
+  final NMarker? marker;
 
-  FoodMapChildWidget({super.key, required this.marker});
+  const FoodMapChildWidget({super.key, this.marker});
 
   @override
-  State<FoodMapChildWidget> createState() => _FoodMapChildWidgetState(marker);
+  _FoodMapChildWidgetState createState() => _FoodMapChildWidgetState();
 }
 
 class _FoodMapChildWidgetState extends State<FoodMapChildWidget> {
-  late NMarker _marker;
-  NaverMapController? _mapController;
-
-  _FoodMapChildWidgetState(NMarker marker) {
-    this._marker = marker;
-  }
+  late Future<NLatLng> _futureLatLng;
 
   @override
   void initState() {
     super.initState();
-    _setLocation();
+    _futureLatLng = _setLocation();
   }
 
-  _setLocation() async {
+  Future<NLatLng> _setLocation() async {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('permissions are denied');
+        return const NLatLng(37.5666, 126.979); // 위치 동의 거부 시 서울 시청 좌표 반환
       }
     }
 
     Position position = await Geolocator.getCurrentPosition();
-    _setMapController(position);
-  }
-
-  _setMapController(Position position) {
-    setState(() {
-      if (_mapController != null) {
-        _mapController!.updateCamera(
-          NCameraUpdate.withParams(
-              target: NLatLng(position.latitude, position.longitude)),
-        );
-      }
-    });
+    return NLatLng(position.latitude, position.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    return NaverMap(
-      options: const NaverMapViewOptions(
-        initialCameraPosition: NCameraPosition(
-          target: NLatLng(37.5666, 126.979),
-          zoom: 15,
-          bearing: 0,
-          tilt: 0,
-        ),
-        zoomGesturesEnable: true,
-        extent: NLatLngBounds(
-          southWest: NLatLng(31.43, 122.37),
-          northEast: NLatLng(44.35, 132.0),
-        ),
-      ),
-      onMapReady: (NaverMapController controller) {
-        _mapController = controller;
-        _mapController!.addOverlay(_marker);
-      },
-
-      onMapTapped: (NPoint point, NLatLng latLng) {
+    return FutureBuilder<NLatLng>(
+      future: _futureLatLng,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          return NaverMap(
+            options: NaverMapViewOptions(
+              initialCameraPosition: NCameraPosition(
+                target: snapshot.data!,
+                zoom: 15,
+                bearing: 0,
+                tilt: 0,
+              ),
+              zoomGesturesEnable: true,
+              extent: const NLatLngBounds(
+                southWest: NLatLng(31.43, 122.37),
+                northEast: NLatLng(44.35, 132.0),
+              ),
+            ),
+            onMapReady: (NaverMapController controller) {
+              if (widget.marker != null) {
+                controller.addOverlay(widget.marker!);
+              }
+            },
+            onMapTapped: (NPoint point, NLatLng latLng) {},
+          );
+        } else {
+          return const Center(child: Text('Unknown error'));
+        }
       },
     );
   }
