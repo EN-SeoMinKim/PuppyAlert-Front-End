f7dedf850d8da336ce69dd8589f114423fb5bb24
Seo SangHyeok <tkdgur9799@gmail.com>
2024-07-29 01:41:08 +0900

    Fix : 관심호스트와의 식사 시간을 null로 받았을 때의 예외처리 구문과 time parsing 로직

--- puppy_alert/lib/models/favorite_host_model.dart
+++ puppy_alert/lib/models/favorite_host_model.dart
@@ -7,11 +7,26 @@ class FavoriteHostModel {
         _recentFoodTime = recentFoodTime;
 
   factory FavoriteHostModel.fromJson(Map<String, dynamic> json) {
-    return FavoriteHostModel(
+    if (json['recentFoodTime'] == null) {
+      return FavoriteHostModel(
         hostId: json['hostId'],
-        recentFoodTime: json['recentFoodTime']);
+        recentFoodTime: '함꼐 식사한 기록이 없습니다'
+      );
+    }
+
+    List<String> timeSplit = json['recentFoodTime'].split('T');
+    String monthDay =
+        '${timeSplit[0].split('-')[1]}/${timeSplit[0].split('-')[2]}';
+    String hourMinute =
+        '${timeSplit[1].split(':')[0]}:${timeSplit[1].split(':')[1]}';
+
+    json['recentFoodTime'] = '$monthDay $hourMinute';
+
+    return FavoriteHostModel(
+        hostId: json['hostId'], recentFoodTime: json['recentFoodTime']);
   }
 
   String get hostId => _hostId;
+
   String get recentFoodTime => _recentFoodTime;
 }
